4554af3604c07df2dc050be79c5a87058eaa3a3a
Sanskriti Seernani <120678365+sfc-gh-sseernani@users.noreply.github.com>
2023-05-25 19:28:29 -0700

    Added Breakfast Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit as sl
 
 sl.title("My Parents' New Healthy Diner")
+
+sl.header("Breakfast Menu")
+sl.text("Omega 3 & Blueberry Oatmeal")
+sl.text("Kale, Spinach & Rocket Smoothie")
+sl.text('Hard-Boiled Free-Range Egg')
