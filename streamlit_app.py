92d04ea47ad108483dba0d76c6c8e2ac19a5c400
Sanskriti Seernani <120678365+sfc-gh-sseernani@users.noreply.github.com>
2023-05-30 13:38:02 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,8 @@ sl.text('🐔 Hard-Boiled Free-Range Egg')
 sl.text("🥑🍞 Avocado Toast")
 
 sl.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 sl.multiselect("Pick some fruits:", list(my_fruit_list.index))
 sl.dataframe(my_fruit_list)
