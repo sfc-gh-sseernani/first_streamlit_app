e25ab70c71bfd013ac067f18a19d65a62f8e563a
Sanskriti Seernani <120678365+sfc-gh-sseernani@users.noreply.github.com>
2023-05-30 13:56:44 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,9 +12,13 @@ sl.text("🥑🍞 Avocado Toast")
 sl.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')  # set index to filter by fruit column
-# sl.multiselect("Pick some fruits:", list(my_fruit_list.index))  without pre-set fruits
-sl.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])  # Added pre-set fruit to show user 
-sl.dataframe(my_fruit_list)
 
+# set index to filter by fruit column
+my_fruit_list = my_fruit_list.set_index('Fruit')  
 
+# without pre-set fruits
+# sl.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+fruits_selected = sl.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+sl.dataframe(fruits_to_show)
