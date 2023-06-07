4da2f12a8c7cb8d5f5531dc3a9d0de4ddc6540a5
Sanskriti Seernani <120678365+sfc-gh-sseernani@users.noreply.github.com>
2023-06-06 20:18:48 -0700

    Update streamlit_app.py
    fruityvice call

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as sl
 import pandas as pd
+import requests
 
 sl.title("My Mom's New Healthy Diner")
 
@@ -22,3 +23,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = sl.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 sl.dataframe(fruits_to_show)
+
+#  fruityvice api response
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
