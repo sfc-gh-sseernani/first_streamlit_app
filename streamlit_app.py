063fe01b679fd122fbd1d38feb2e08c4575e5104
Sanskriti Seernani <120678365+sfc-gh-sseernani@users.noreply.github.com>
2023-06-06 22:30:11 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,14 +28,16 @@ sl.dataframe(fruits_to_show)
 
 #  fruityvice api response
 sl.header("Fruityvice Fruit Advice!")
-fruit_choice = sl.text_input('What fruit would you like information about?', 'Kiwi')
-sl.write('The user entered', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-# sl.text(fruityvice_response.json())
-
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-sl.dataframe(fruityvice_normalized)
+try:
+  fruit_choice = sl.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    sl.error("Please select a fruit to get information")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    sl.dataframe(fruityvice_normalized)
+except URLError as e:
+  sl.error()
 
 sl.stop()
 
