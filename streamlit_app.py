9eefb53cf11174a104c4dee1b50d2e0fcb315297
Sanskriti Seernani <120678365+sfc-gh-sseernani@users.noreply.github.com>
2023-06-06 20:31:08 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,10 @@ sl.dataframe(fruits_to_show)
 
 #  fruityvice api response
 sl.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruit_choice = sl.text_input('What fruit would you like information about?', 'Kiwi')
+sl.write('The user entered', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # sl.text(fruityvice_response.json())
 
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
