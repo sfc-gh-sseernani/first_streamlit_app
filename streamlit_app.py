ef38545b133596172cda65c07014642eeefd09c7
Sanskriti Seernani <120678365+sfc-gh-sseernani@users.noreply.github.com>
2023-06-06 20:27:15 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,8 @@ sl.dataframe(fruits_to_show)
 
 #  fruityvice api response
 sl.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-sl.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+# sl.text(fruityvice_response.json())
 
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 sl.dataframe(fruityvice_normalized)
