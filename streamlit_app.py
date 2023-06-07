a23f8f05f960a76abf6088cf9078c2540d380a17
Sanskriti Seernani <120678365+sfc-gh-sseernani@users.noreply.github.com>
2023-06-06 20:20:23 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as sl
 import pandas as pd
-import requests
 
 sl.title("My Mom's New Healthy Diner")
 
@@ -25,5 +24,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 sl.dataframe(fruits_to_show)
 
 #  fruityvice api response
+import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+sl.text(fruityvice_response)
